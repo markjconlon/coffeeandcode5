ecb4f01a0571a1eb98c6aaf2afadf601f7c7686b
Mark <markjconlon@gmail.com>
2017-04-11 10:05:03 -0400

    finished the 3rd problem

---
+++ luck_check.rb
@@ -0,0 +1,20 @@
+def luck_check(num)
+  str= num.to_s
+  array= str.split("")
+  x = str.length / 2
+  sum_start = 0
+  sum_end = 0
+  array[0..x].each do |num|
+    sum_start += num.to_i
+  end
+  array[-x..-1].each do |num|
+    sum_end += num.to_i
+  end
+  if sum_end == sum_end
+    return "Woohoo it is lucky!"
+  else
+    return "This is not lucky!"
+  end
+end
+
+puts luck_check(003111)
--- valid_phone_numbers.rb
+++ valid_phone_numbers.rb
@@ -1,5 +1,21 @@
 def valid_phone_numbers(str)
-  str.to_a!
+  numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  array = str.split("")
+  if array[0] == "(" && array[4] == ")" && array[5] && array[9] == "-"
+    array.delete_at(9)
+    array.delete_at(5)
+    array.delete_at(4)
+    array.delete_at(0)
+    array.each do |num|
+      if num.is_a? Integer
+      end
+    return true
+    end
+  else
+    return false
+  end
 end
 
-put valid_phone_numbers("(123) 456-7890")
+puts valid_phone_numbers("(123) 456-7890")
+puts valid_phone_numbers("(1111)555 2345") # => returns false
+puts valid_phone_numbers("(098) 123 4567") # => returns false
